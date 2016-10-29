7684ac24fd752b083c5315c2597ef937428a194b
r3c0d3x <r3c0d3x@gmail.com>
2016-10-29 16:08:22 -0400

    Use streams to write threads

--- 1_get_threads.js
+++ 1_get_threads.js
@@ -6,25 +6,31 @@ const config = require('./config.json');
 const async = require('async');
 const fs = require('fs-extra');
 
-async.eachSeries(config.boards, (board, boardCb) => {
-    let page = 1;
-    let threads = [];
-    async.doWhilst((pageCb) => api.page(board, page, (err, data) => pageCb(err, data)), (data) => {
-        console.log('Downloaded page ' + page + ' for /' + board + '/');
-        if (typeof data.length === 'undefined') {
-            threads = threads.concat(Object.getOwnPropertyNames(data).map((num) => { return num * 1; }));
-            page++;
-            return true;
-        } else if (typeof data.length !== 'undefined' && data.length === 0)
-            return false;
-    }, (err) => {
-        if (err === null)
-            fs.outputFile('data/' + board + '.json', JSON.stringify(threads), (fileErr) => {
-                boardCb(fileErr);
+fs.ensureDir('data/', (dirErr) => {
+    if (dirErr === null)
+        async.eachSeries(config.boards, (board, boardCb) => {
+            let page = 1;
+            let stream = fs.createWriteStream('data/' + board + '.json');
+            stream.write('[');
+            async.doWhilst((pageCb) => api.page(board, page, (err, data) => pageCb(err, data)), (data) => {
+                console.log('Downloaded page ' + page + ' for /' + board + '/');
+                if (typeof data.length === 'undefined') {    
+                    let out = Object.getOwnPropertyNames(data).map((num) => { return num * 1; }).join(',');
+                    if (page > 1)
+                        out = ',' + out;
+                    stream.write(out);
+                    page++;
+                    return true;
+                } else if (typeof data.length !== 'undefined' && data.length === 0)
+                    return false;
+            }, (err) => {
+                stream.write(']');
+                stream.end();
+                boardCb(err);
             });
-        else
-            boardCb(err);
-    });
-}, (err) => {
-    console.log('\nDone!\n');
+        }, (err) => {
+            console.log('\nDone!\n');
+        });
+    else
+        console.log(err);
 });
