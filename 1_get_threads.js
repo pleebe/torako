ee1b7670c365f2a7bbc08ead2b4ed1d2f4d28fcf
r3c0d3x <r3c0d3x@gmail.com>
2016-10-31 16:46:13 -0400

    Added ability to continue previous runs

--- 1_get_threads.js
+++ 1_get_threads.js
@@ -6,31 +6,60 @@ const config = require('./config.json');
 const async = require('async');
 const fs = require('fs-extra');
 
-fs.ensureDir('data/', (dirErr) => {
-    if (dirErr === null)
-        async.eachSeries(config.boards, (board, boardCb) => {
-            let page = 1;
-            let stream = fs.createWriteStream('data/' + board + '.json');
-            stream.write('[');
-            async.doWhilst((pageCb) => api.page(board, page, (err, data) => pageCb(err, data)), (data) => {
-                console.log('Downloaded page ' + page + ' for /' + board + '/');
-                if (typeof data.length === 'undefined') {    
-                    let out = Object.getOwnPropertyNames(data).map((num) => { return num * 1; }).join(',');
-                    if (page > 1)
-                        out = ',' + out;
-                    stream.write(out);
-                    page++;
-                    return true;
-                } else if (typeof data.length !== 'undefined' && data.length === 0)
-                    return false;
+let recovery;
+let recovered = false;
+let boards = config.boards;
+fs.readJson('status.json', (statusReadErr, data) => {
+    if (statusReadErr === null) {
+        console.log('Continuing from previous recovery point (see status.json)...\n');
+        recovery = data;
+        recovered = true;
+    }
+    
+    if (recovered)
+        boards = boards.slice(boards.indexOf(recovery.board));
+
+    fs.ensureDir('data/', (dirErr) => {
+        if (dirErr === null)
+            async.eachSeries(boards, (board, boardCb) => {
+                let page = 1;
+                console.log(recovered);
+                console.log(board);
+                console.log(recovery.board);
+                console.log(board === recovery.board);
+                if (recovered && board === recovery.board)
+                    page = recovery.page + 1;
+                let stream = fs.createWriteStream('data/' + board + '.json');
+                stream.write('[');
+                async.doWhilst((pageCb) => api.page(board, page, (err, data) => pageCb(err, data)), (data) => {
+                    if (typeof data.length === 'undefined') {    
+                        let out = Object.getOwnPropertyNames(data).map((num) => { return num * 1; }).join(',');
+                        if (page > 1)
+                            out = ',' + out;
+                        stream.write(out);
+                        fs.outputFileSync('status.json', JSON.stringify({ board: board, page: page }));
+                        console.log('Downloaded page ' + page + ' for /' + board + '/');
+                        page++;
+                        return true;
+                    } else if (typeof data.length !== 'undefined' && data.length === 0) {
+                        fs.outputFileSync('status.json', JSON.stringify({ board: board, page: page }));
+                        console.log('Downloaded page ' + page + ' for /' + board + '/');
+                        return false;
+                    }
+                }, (err) => {
+                    if (err === null)
+                        stream.write(']');
+                    stream.end();
+                    boardCb(err);
+                });
             }, (err) => {
-                stream.write(']');
-                stream.end();
-                boardCb(err);
+                fs.remove('status.json', (err) => {
+                    if (err !== null)
+                        console.log(err);
+                });
+                console.log('\nDone!\n');
             });
-        }, (err) => {
-            console.log('\nDone!\n');
-        });
-    else
-        console.log(err);
+        else
+            console.log(err);
+    });
 });
